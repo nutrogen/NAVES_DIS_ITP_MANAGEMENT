b2c696b50f19bed9c40d343f10e50f4711956e1d
nutrogen <81790274+nutrogen@users.noreply.github.com>
2023-05-01 12:58:03 +0900

    Add files via upload

---
+++ DisItpManagementController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using NavesPortalforWebWithCoreMvc.Controllers.AuthFromIntranetController;
+
+namespace NavesPortalforWebWithCoreMvc.Controllers.DIS
+{
+    [Authorize]
+    [CheckSession]
+    public class DisItpManagementController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Title = "Project Information";
+            return View();
+        }
+    }
+}
